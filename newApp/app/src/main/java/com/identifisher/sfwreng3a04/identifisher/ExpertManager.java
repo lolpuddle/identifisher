8d095ab856f6774bf7bd2f476309c5a1cee70b7c
Tian Guo <guotq@mcmaster.ca>
2016-04-05 12:47:16 -0400

    Add sort and removeDup methods for expert manager

--- newApp/app/src/main/java/com/identifisher/sfwreng3a04/identifisher/ExpertManager.java
+++ newApp/app/src/main/java/com/identifisher/sfwreng3a04/identifisher/ExpertManager.java
@@ -1,8 +1,17 @@
 package com.identifisher.sfwreng3a04.identifisher;
 
 import android.util.Log;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import sql.DBHelper;
 
@@ -56,9 +65,60 @@ public class ExpertManager {
             }
         }
         String[] toSort = allFishInOne.toArray(new String[allFishInOne.size()]);
-        return overlap(allFish.toArray(new String[allFish.size()][maxCol]));
+        toSort = removeDup(sort(toSort));
+        return toSort;
+        //return overlap(allFish.toArray(new String[allFish.size()][maxCol]));
     }
 
+    /**sort by frequency. Reference: http://www.java-fries.com/2015/02/sort-elements-frequency/
+     * Sort by creating a map with element in the targeted array as keys and the frequency as value
+     * Convert the sorted map back to an array.
+     * @param arr - list of unordered fish returned by all experts
+     * @return String[] - list of fish ordered from most to least frequency of occurrence
+     */
+    private String[] sort(String[] arr) {
+    // map of the elements of array and frequency as its value
+        Map<String, Integer> map = new LinkedHashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            String key = arr[i];
+            if (map.containsKey(key)) {
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key, 1);
+            }
+        }
+        //sort list of elements of map's entries
+        List<Entry<String, Integer>> entryList = new ArrayList<Entry<String, Integer>>(map.entrySet());
+        Collections.sort(entryList, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Entry<String, Integer> s1,
+                               Entry<String, Integer> s2) {
+                return s2.getValue().compareTo(s1.getValue());
+            }
+        });
+        //rearrange array
+        int index = 0;
+        for (Map.Entry<String, Integer> entry : entryList) {
+            for (int i = 0; i < entry.getValue(); i++) {
+                arr[index++] = entry.getKey();
+            }
+        }
+        return arr;
+    }
+
+    /**
+     * Provides the means of removing duplicated entries in an array
+     * Use hash set to eliminate non-unique entries
+     * @param arr - list of fish
+     * @return String[] - list of fish without duplicates
+     */
+    private String[] removeDup(String[] arr) {
+        List<String> temp = Arrays.asList(arr);
+        Set<String> set = new HashSet<String> (temp);
+        String[] newArr = new String[set.size()];
+        set.toArray(newArr);
+        return newArr;
+    }
     /**
      * Provides the means of finding any overlapping fish in all of the arrays of allFish
      * @param allFish - list from each Expert on what fish they think it is
